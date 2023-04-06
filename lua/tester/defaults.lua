4b131bbb5d25efca655cd219fbc7f8a7052fc87a
Al0den <111601320+Al0den@users.noreply.github.com>
2023-04-06 11:41:08 +0200

    fix:Remove default ml file content, useless and annoying

--- lua/tester/defaults.lua
+++ lua/tester/defaults.lua
@@ -7,7 +7,6 @@ M.defaultContent = {
     py = 'def main():\n    @\n\n\nif __name__ == "__main__":\n    main()\n',
     html =
     '<!DOCTYPE html>\n<html lang="en">\n  <head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <meta http-equiv="X-UA-Compatible" content="ie=edge">\n    <title>Testing</title>\n    <link rel="stylesheet" href="style.css">\n  </head>\n  <body>\n     @\n  </body>\n</html>',
-    ml = 'let print_bool val = match val with\n    | true -> print_string"true"\n    | false -> print_string"false"\n@\n'
 }
 M.writeDir = "current"
 M.formatOnOpen = false
